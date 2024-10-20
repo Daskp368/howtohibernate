4c56a9bdc434223590608b87a737753eefdc63e1
Daskp368 <126821150+Daskp368@users.noreply.github.com>
2024-10-20 14:53:55 +0400

    PetRepository.java

--- src/main/java/com/example/myapp/repository/PetRepository.java
+++ src/main/java/com/example/myapp/repository/PetRepository.java
@@ -1,2 +1,9 @@
-package com.example.myapp.repository;public class PetRepository {
+package com.example.myapp.repository;
+
+import com.example.myapp.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
 }
