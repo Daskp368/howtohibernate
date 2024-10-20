db7d658c41d3ee48adf46ae91272a78acf26ad44
Daskp368 <126821150+Daskp368@users.noreply.github.com>
2024-10-20 14:54:15 +0400

    OwnerRepository.java

--- src/main/java/com/example/myapp/repository/OwnerRepository.java
+++ src/main/java/com/example/myapp/repository/OwnerRepository.java
@@ -1,2 +1,9 @@
-package com.example.myapp.repository;public class OwnerRepository {
+package com.example.myapp.repository;
+
+import com.example.myapp.model.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
 }
