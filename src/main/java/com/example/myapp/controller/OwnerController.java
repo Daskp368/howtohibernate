fc5f40dd7a275b446595febec307ddce2016f725
Daskp368 <126821150+Daskp368@users.noreply.github.com>
2024-10-20 14:49:48 +0400

    OwnerController.java

--- src/main/java/com/example/myapp/controller/OwnerController.java
+++ src/main/java/com/example/myapp/controller/OwnerController.java
@@ -1,2 +1,46 @@
-package com.example.myapp.controller;public class OwnerController {
+package com.example.myapp.controller;
+
+import com.example.myapp.model.Owner;
+import com.example.myapp.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/owner")
+public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @GetMapping
+    public List<Owner> getAllOwners() {
+        return ownerService.getAllOwners();
+    }
+
+    @GetMapping("/id")
+    public Owner getOwnerById(@PathVariable Long id) {
+        return ownerService.getOwnerById(id);
+    }
+
+    @PostMapping
+    public Owner createOwner(@RequestBody Owner owner) {
+        return ownerService.createOwner(owner);
+    }
+
+    @PutMapping("/{id}")
+    public Owner updateOwner(@PathVariable Long id, @RequestBody Owner owner) {
+        return ownerService.updateOwner(owner);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOwner(@PathVariable Long id) {
+        ownerService.deleteOwner(id);
+    }
+
 }
